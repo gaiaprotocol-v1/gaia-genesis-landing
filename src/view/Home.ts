cd7231116edb5e286b768ab6e6c24e02e3b2680f
dilrong <dilrong@dilrong.com>
2022-01-23 23:11:27 +0900

    styles: 디자인 업데이트 약간의 디자인 보완

--- src/view/Home.ts
+++ src/view/Home.ts
@@ -10,76 +10,46 @@ export default class Home implements View {
     constructor() {
         Layout.current.content.append(
             this.container = el(".home-view flex h-screen flex-col",
-                el(".m-auto flex flex-col items-center m-4", { id: "STORY" },
+                el(".m-auto flex flex-col items-center m-4 container mx-auto px-4", { id: "STORY" },
                     el("img.block lg:hidden mt-16 content-center", { src: "/images/logo-text.png", alt: "gaia protocol logo" }),
                     el("img.hidden lg:block w-1/3 mt-16 content-center", { src: "/images/logo-text.png", alt: "gaia protocol logo" }),
-                    el(".story content-center mt-32",
-                        el("h1.text-5xl font-bold text-center", "What is Gaia PFP NFTs?"),
-                        el(".text-base text-center", `
-                        Gaia Protocol은 최초의 투자형 PFP(프로필 이미지) NFT 프로젝트입니다. 
-                        
-                        투자자들의 초기 투자비용을 최대한 보장하고 지지(Backing)합니다. 
-                        
-                        NFT 투자자들은 수준 높은 커뮤니티를 구성하고 매력적인 문화를 만들고 최고의 NFT 허브를 구축해 나갑니다.`)
+                    el(".story content-center mt-4",
+                        el(".text-base text-secondary drop-shadow-sm leading-7 text-center", "Gaia Protocol은 최초의 투자형 PFP(프로필 이미지) NFT 프로젝트입니다.\n투자자들의 초기 투자비용을 최대한 보장하고 지지(Backing)합니다.\nNFT 투자자들은 수준 높은 커뮤니티를 구성하고 매력적인 문화를 만들고 최고의 NFT 허브를 구축해 나갑니다.")
                     ),
-                    el(".deFi2.0 content-center mt-32", { id: "DeFi 2.0" },
-                        el("h1.text-5xl font-bold text-center", "Why DeFi 2.0"),
-                        el(".text-base text-center", `
-                        DeFi 2.0 은 창의적인 방법으로 유동성을 프토토콜이 확보하고, 강력한 커뮤니티 파워에 힘입어 투자들의 이윤의 극대화를 일구어낸 성공적인 프로젝트입니다. 
-                        
-                        Gaia Protocol 의 지지하는 핵심은 NFT를 구매한 투자자들의 자산을 이에 예치함으로써 투자자들이 영구적으로 혜택을 누릴 수 있도록 하는 것입니다.`),
+                    el(".deFi2.0 content-center mt-64", { id: "DeFi 2.0" },
+                        el("h1.text-5xl font-bold text-center text-primary", "WHY DeFi2.0?"),
+                        el(".text-base text-center mt-8 leading-7", "DeFi 2.0 은 창의적인 방법으로 유동성을 프토토콜이 확보하고,\n강력한 커뮤니티 파워에 힘입어 투자들의 이윤의 극대화를 일구어낸 성공적인 프로젝트입니다.\n Gaia Protocol 의 지지하는 핵심은 NFT를 구매한 투자자들의 자산을 이에 예치함으로써 투자자들이 영구적으로 혜택을 누릴 수 있도록 하는 것입니다."),
                     ),
-                    el(".buyback content-center mt-32", { id: "BUYBACK FUND" },
-                        el("h1.text-5xl font-bold text-center", "BUYBACK FUND"),
-                        el(".text-base text-center", `
-                        Buyback Fund 는 민팅액의 45%로 구성되며 추후 NFT의 오픈씨 거래 수수료(로열티)가 배분됩니다. 
-                        
-                        민팅으로 NFT 를 구매한 사용자는 최초 민팅가에서 수령한 이자 수익을 제한 금액을 바이백 펀드를 통해 청구할 수 있고 NFT 는 팀이 거래소를 통해 재판매합니다.`),
+                    el(".buyback content-center mt-64", { id: "BUYBACK FUND" },
+                        el("h1.text-5xl font-bold text-center text-primary", "BUYBACK FUND"),
+                        el(".text-base text-center mt-8 leading-7", "Buyback Fund 는 민팅액의 45%로 구성되며 추후 NFT의 오픈씨 거래 수수료(로열티)가 배분됩니다.\n민팅으로 NFT 를 구매한 사용자는 최초 민팅가에서 수령한 이자 수익을 제한 금액을 바이백 펀드를 통해 청구할 수 있고 NFT 는 팀이 거래소를 통해 재판매합니다."),
                     ),
-                    el(".mint content-center mt-32", { id: "MINT" },
-                        el("h1.text-5xl font-bold text-center", "MINT"),
-                        el("h2.text-3xl font-bold mt-8  text-center", "Pre-Sale (Whitelist)"),
+                    el(".gaiaNFT content-center mt-64", { id: "SNEAK PEEK" },
+                        el("h1.text-5xl font-bold text-center text-primary", "SNEAK PEEK"),
+                        el(".swiper-container mt-8 grid grid-cols-2 gap-4",
+                            el(".swiper-slide",
+                                el("img.w-64 rounded", { src: "/images/nft/sneakpeek1.png" })
+                            ),
+                            el(".swiper-slide",
+                                el("img.w-64 rounded", { src: "/images/nft/sneakpeek2.png" })
+                            )
+                        ),
+                        el(".text-xs text-center mt-4 leading-7", "본 이미지는 스닉픽으로 실제 NFT 민팅되는 NFT와 다를 수 있습니다."),
+                    ),
+                    el(".mint content-center mt-64", { id: "MINT" },
+                        el("h1.text-5xl font-bold text-center text-primary", "MINT"),
+                        el("h2.text-3xl font-bold mt-8  text-center", "Pre Sale"),
+                        el(".text-xs text-center font-medium", "(White List)"),
                         el("h3.text-2xl mt-8 text-center font-medium", "2월 10일 밤 10:00시"),
                         el(".text-gray-300 text-base mt-2 text-center", "1,000 Klay"),
                         el(".text-gray-300 text-base mt-2 text-center", "총 4,000개"),
-                        el("h2.text-3xl font-bold mt-8  text-center", "Public-Sale"),
+                        el("h2.text-3xl font-bold mt-8  text-center", "Public Sale"),
                         el("h3.text-2xl mt-8 text-center font-medium", "2월 10일 밤 11:00시"),
                         el(".text-gray-300 text-base mt-2 text-center", "1,000 Klay"),
                         el(".text-gray-300 text-base mt-2 text-center", "총 6,000 개"),
                     ),
-                    /*el(".whiteList content-center mt-32", { id: "WHITELIST" },
-                        el("h1.text-5xl font-bold text-center", "WHITELIST"),
-                        el(".text-base text-center", `
-                        - 커뮤니티 빌딩
-
-                        - 민팅
-
-                        - Kronos DAO 예치
-
-                        - 이자 채굴 시스템 오픈
-
-                        - 바이백 프로토콜 개시
-
-                        - 이자 수익 원금 상환 축하 파티 개최ß
-                        
-                        - Olympus DAO를 사용하는 V2 발행 (3D)
-                        `),
-                    ),
-                    el(".whiteList content-center mt-32", { id: "ROADMAP" },
-                        el("h1.text-5xl font-bold text-center", "ROADMAP"),
-                        el(".text-base text-center", `
-                    1. 화리 물량 총 4,000개 
-
-                    2. 해당 자산 1천클에 화리 1개, 인당 5개
-                    
-                    3. 24일 타입폼으로 신청 받기
-
-                    4. 2월 9일까지 자산 유지 필수, 민팅 때 자산 재체크 들어감
-
-                    5. 1월 21일 금요일 공지`),
-                    ),*/
-                    el(".team content-center mt-32", { id: "TEAM" },
-                        el("h1.text-5xl font-bold text-center", "TEAM"),
+                    el(".team content-center mt-64", { id: "TEAM" },
+                        el("h1.text-5xl font-bold text-center text-primary", "TEAM"),
                         el("h3.text-3xl font-medium text-center mt-8", "Director 심영재"),
                         el(".text-base text-center mt-8",
                             `그는 국내 최초로 PFP를 만든 DSC의 개발자이자, 클레이튼 최초의 밈코인 인절미를 만든 떡방앗간 커뮤니티의 창시자이기도 합니다. 
@@ -111,9 +81,46 @@ export default class Home implements View {
                         el("h3.text-3xl font-medium text-center mt-8", "Art & Design 조선우"),
                         el(".text-base text-center mt-8 mb-8",
                             `현직 게임 개발자이자 NFT 아티스트입니다. 
-                            다년간 트위터 작품 활동을 통해 32만 팔로워를 보유하고 있으며, 최근에는 한국의 NFT 및 P2E 게임 프로젝트에도 참여하고 있습니다.`
+                            다년간 트위터 작품 활동을 통해 32만 팔로워를 보유하고 있으며, 
+                            최근에는 한국의 NFT 및 P2E 게임 프로젝트에도 참여하고 있습니다.`
                         )
                     ),
+                    el(".partner content-center mt-64", { id: "PARTNER" },
+                        el("h1.text-5xl font-bold text-center text-primary", "PARTNER"),
+                        el(".partner-container mt-16 flex md:flex-col lg:flex-row",
+                            el("img.rounded h-16 md:mt-8 lg:mr-16", { src: "/images/partner/kronosDAO.svg" }),
+                            el("img.rounded h-16 md:mt-8 lg:mr-16", { src: "/images/partner/dexata.svg" }),
+                            el("img.rounded h-16 md:mt-8", { src: "/images/partner/klayswap.svg" })
+                        ),
+                    )
+                ),
+                el("footer.bg-black p-8 mt-32",
+                    el(".footer-container.container mx-auto px-4 flex justify-around",
+                        el(".resources-container text-left text-xs flex flex-col",
+                            el(".head.text-xl mb-2 font-bold", "Resources"),
+                            el(".content flex flex-col text-gray-300",
+                                el("a.documentation hover:underline mt-2", "Documentation", { href: "https://docs.gaiaprotocol.com/kr/", target: "_blank" }),
+                            )
+                        ),
+                        el(".sns-container text-left text-xs",
+                            el(".head.text-xl mb-2 font-semibold", "Community"),
+                            el(".content flex text-gray-300 lg:flex-row mt-4",
+                                el("a.discord.mr-4", { href: "https://discord.com/invite/SjM4meh3hd", target: "_blank" },
+                                    el("img", { src: "/images/community/discord.svg" })
+                                ),
+                                el("a.telegram mr-4", { href: "https://t.me/gaiaprotocol", target: "_blank" },
+                                    el("img", { src: "/images/community/telegram.svg" })
+                                ),
+                                el("a.twitter mr-4", { href: "https://twitter.com/gaia_protocol", target: "_blank" },
+                                    el("img", { src: "/images/community/twitter.svg" })
+                                ),
+                                el("a.kakaotalk mr-4", { href: "https://open.kakao.com/o/ggBYKEUd", target: "_blank" },
+                                el("img", { src: "/images/community/kakao-talk.svg" })
+                                ),
+                            )
+                        ),
+                    ),
+                    el(".text-xs text-center mt-16", "COPYRIGHT ⓒ Gaia Protocol. ALL RIGHTS RESERVED")
                 )
             ).appendTo(BodyNode)
         )
--- src/view/Layout.ts
+++ src/view/Layout.ts
@@ -12,33 +12,34 @@ export default class Layout implements View {
 
         BodyNode.append(
             (this.container = el(".layout",
-                el("header.bg-black fixed top-0 left-0 right-0 .hidden lg:block",
-                    el("nav",
-                        el(".max-w-7xl mx-auto px-2 sm:px-6 lg:px-8",
+                el("header.bg-black fixed top-0 left-0 right-0 hidden lg:block",
+                    el("nav.container mx-auto px-4",
+                        el(".max-w-8xl",
                             el(".relative flex items-center justify-between h-16",
                                 el(".flex-1 flex items-center justify-center sm:items-stretch sm:justify-start",
                                     el(".flex-shrink-0 flex items-center",
-                                        el("img.block lg:hidden h-8 w-auto", { src: "/images/logo.png", alt: "gaia protocol logo" }),
-                                        el("img.hidden lg:block h-8 w-auto", { src: "/images/logo.png", alt: "gaia protocol logo" }),
+                                        el("a", { href: "#STORY" },
+                                            el("img.block lg:hidden h-8 w-auto", { src: "/images/logo.png", alt: "gaia protocol logo" })
+                                        ),
+                                        el("a", { href: "#STORY" },
+                                            el("img.hidden lg:block h-8 w-auto", { src: "/images/logo.png", alt: "gaia protocol logo" })
+                                        )
                                     )
                                 ),
                                 el(".hidden sm:block sm:ml-6",
                                     el(".flex space-x-4"),
-                                    el("a.text-gray-300 hover:bg-gray-400 hover:text-white px-3 py-2 rounded-md text-sm font-medium", "WHAT", { href: "#STORY" }),
                                     el("a.text-gray-300 hover:bg-gray-400 hover:text-white px-3 py-2 rounded-md text-sm font-medium", "WHY", { href: "#DeFi 2.0" }),
                                     el("a.text-gray-300 hover:bg-gray-400 hover:text-white px-3 py-2 rounded-md text-sm font-medium", "BUYBACK FUND", { href: "#BUYBACK FUND" }),
+                                    el("a.text-gray-300 hover:bg-gray-400 hover:text-white px-3 py-2 rounded-md text-sm font-medium", "SNEAK PEEK", { href: "#SNEAK PEEK" }),
                                     el("a.text-gray-300 hover:bg-gray-400 hover:text-white px-3 py-2 rounded-md text-sm font-medium", "MINT", { href: "#MINT" }),
-                                    //el("a.text-gray-300 hover:bg-gray-400 hover:text-white px-3 py-2 rounded-md text-sm font-medium", "WHITELIST", { href: "#WHITELIST" }),
-                                    //el("a.text-gray-300 hover:bg-gray-400 hover:text-white px-3 py-2 rounded-md text-sm font-medium", "ROADMAP", { href: "#ROADMAP" }),
                                     el("a.text-gray-300 hover:bg-gray-400 hover:text-white px-3 py-2 rounded-md text-sm font-medium", "TEAM", { href: "#TEAM" }),
+                                    el("a.text-gray-300 hover:bg-gray-400 hover:text-white px-3 py-2 rounded-md text-sm font-medium", "PARTNER", { href: "#PARTNER" }),
                                 )
                             ),
                         )
                     ),
                 ),
                 el("main", (this.content = el(".content"))),
-                el("footer",
-                )
             ))
         );
     }
