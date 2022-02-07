7144a6ab18839e9eef6b470188cf5a6ae5f8fe61
dilrong <dilrong@dilrong.com>
2022-02-08 01:48:00 +0900

    fix: 중국어 번역 밸류값 수정

--- src/view/Landing.ts
+++ src/view/Landing.ts
@@ -34,8 +34,8 @@ export default class Landing implements View {
               el("li.item", select = el("select.language-select",
                 el("option", "한국어 🇰🇷 ", { value: "ko" }),
                 el("option", "English 🇺🇸 ", { value: "en" }),
-                el("option", "繁體字 🇨🇳 ", { value: "zh-SC" }),
-                el("option", "简体字 🇨🇳 ", { value: "zh-TC" }),
+                el("option", "繁體字 🇨🇳 ", { value: "zh-TC" }),
+                el("option", "简体字 🇨🇳 ", { value: "zh-SC" }),
                 {
                   change: () => {
                     BrowserInfo.changeLanguage(select.domElement.value);
